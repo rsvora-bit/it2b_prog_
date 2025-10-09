338be676e2af58e38be2219abda2ded98029e125
rsvora-bit <roman.svora.ml@gmail.com>
2025-10-09 09:39:40 +0200

    dalsi programy z hodin

---
+++ moje_kody/math_h.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+
+int main(){
+
+    printf("Zaukrohleno: %d", (int)round(5.67));
+    printf("Zaukrohleno: %d", (int)floor(5.67));
+    printf("Zaukrohleno: %d", (int)ceil(5.67));
+
+    printf("\n");
+    printf("Odmocnina: %f\n", sqrt(4));
+
+    printf("Cislo pi: %f", M_PI)
+
+    return 0;
+}
